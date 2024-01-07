c2c76fa38022fe9fcb3671ec8f333f0d32caca1c
fearcube69 <hakeemuddinakim44@gmail.com>
2024-01-07 21:56:47 +0800

    some directory changing was added

--- build/wipe.sh
+++ build/wipe.sh
@@ -1,51 +1,28 @@
 #!/bin/bash
 
-#current_user=$(whoami)
-## Initialize an empty array to store the directory names
-#directory_names=()
-
-## List all directories under /media/roxy
-#for dir in /media/"$current_user"/*/; do
-#   # Get the name of the directory
-#   directory_name=$(basename "$dir")
-#
-#   # Add the name of the directory to the array
-#   directory_names+=("$directory_name")
-#done
-#
-## Print the names of the directories
-#for name in "${directory_names[@]}"; do
-#   echo "$name"
-#done
-#
-#target_mount_point="/media/$current_user/$name"
-
-# Define the mount point of the USB flash drive
-# Define the specific directory to be used as the mount point
-target_mount_point="/media/roxy/NGS"
-
-get_mounted_devices() {
-    awk -v target="$target_mount_point" '$2 == target {print "Device: " $1 ", Mount Point: " $2 ", File System Type: " $3}' /proc/self/mounts
+# Function to wipe the mounted drive
+function wipe_drive() {
+ echo "Unmounting the drive..."
+ fuser -k /media/$USERNAME
+ sleep 1
+ echo "Wiping the drive..."
+ mkfs.$FORMAT $MOUNTED_DEVICE
+ echo "Remounting the drive..."
+ mount $MOUNTED_DEVICE /media/$USERNAME
+ echo "The drive has been wiped."
 }
 
-main() {
-    get_mounted_devices
-}
+# Get the username from the whoami command
+USERNAME=$(whoami)
+WIPE="false" # Set to true to wipe the drive
 
-main
-
-# Define the device identifier of the USB flash drive
-device_identifier=$(df "$target_mount_point" | tail -n1 | awk '{print $1}')
-
-# Unmount the USB flash drive
-umount "$device_identifier"
-
-# Format the USB flash drive with the NTFS file system
-mkfs.ntfs "$device_identifier"
-
-# Check the exit status of the mkfs command
-if [ $? -eq 0 ]; then
-   echo "Formatting successful."
+if mountpoint -q "/media/$USERNAME"; then
+ MOUNTED_DEVICE=$(df -h /media/$USERNAME --output=source | tail -n1)
+ FORMAT=$(lsblk -no FSTYPE $MOUNTED_DEVICE)
+ echo "/media/$USERNAME is a mount point with format: $FORMAT"
+ if [ "$WIPE" = "true" ]; then
+     wipe_drive
+ fi
 else
-   echo "Formatting failed."
+ echo "/media/$USERNAME is not a mount point"
 fi
